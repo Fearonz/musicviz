b50da58a25a9756bd024966440032d767a83fe15
Fearon <fearonedm@outlook.com>
2017-03-14 09:55:06 +0000

    Add files via upload

---
+++ music_viz.ino
@@ -0,0 +1,36 @@
+
+/*
+This is the code to make a LED blink with the music.
+You have to set the threshold so it' sensible enough to make the led blink.
+You connect an LED to PIN13 and the Sound Sensor to Analog Pin 0
+ */
+ 
+int led = 13;
+int threshold = 800; //Change This
+int volume;
+
+void setup() {                
+  Serial.begin(9600); // For debugging
+  pinMode(led, OUTPUT);   
+  
+}
+
+void loop() {
+  
+  volume = analogRead(A1); // Reads the value from the Analog PIN A1
+  led = analogRead(A0); // Will Read the vaule on pin A0  
+ 
+  /*
+    //Debug mode
+    Serial.println(volume);
+    delay(100);
+  */
+  
+  if(volume>=threshold){
+    digitalWrite(led, HIGH); //Turn ON Led
+  }  
+  else{
+    digitalWrite(led, LOW); // Turn OFF Led
+  }
+
+}
